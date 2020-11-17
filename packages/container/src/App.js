ed6bfad8064711da91aa62a6bfa3cfc324338896
Yvalain <yvalainj@msn.com>
2020-11-17 14:09:16 +0100

    Updated text after changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return <div>
-        <h1>Hi Everybody over there !!!</h1>
+        <h1>Hello see you after build modification !!!</h1>
         <hr />
         <MarketingApp />
     </div>
