6b719b91b1b69b402a78bdf4762cfe0b1f7da0f3
Yvalain <yvalainj@msn.com>
2020-11-21 19:52:31 +0100

    Added auth to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
@@ -15,7 +16,10 @@ export default () => {
             <BrowserRouter>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Switch>
+                        <Route path="/auth" component={AuthApp}></Route>
+                        <Route path="/" component={MarketingApp}></Route>
+                    </Switch>
                 </div>
             </BrowserRouter>
         </StylesProvider>
