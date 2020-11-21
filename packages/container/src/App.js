91bb602eab5938b6e7bcec4e0d02f4b980d158ea
Yvalain <yvalainj@msn.com>
2020-11-21 22:49:51 +0100

    Added sign in / sign out logic

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
@@ -13,16 +13,19 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
     return (
         <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <div>
-                    <Header />
-                    <Suspense fallback={<Progress/>}>
-                    <Switch>
-                        <Route path="/auth" component={AuthLazy}></Route>
-                        <Route path="/" component={MarketingLazy}></Route>
-                    </Switch>
+                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" component={MarketingLazy}></Route>
+                        </Switch>
                     </Suspense>
                 </div>
             </BrowserRouter>
