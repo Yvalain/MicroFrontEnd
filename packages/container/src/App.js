baa6e307f617fbb9e035a781dda5d7bb0eb1aadf
Yvalain <yvalainj@msn.com>
2020-11-21 20:49:06 +0100

    Added auth deployment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -16,10 +18,12 @@ export default () => {
             <BrowserRouter>
                 <div>
                     <Header />
+                    <Suspense fallback={<Progress/>}>
                     <Switch>
-                        <Route path="/auth" component={AuthApp}></Route>
-                        <Route path="/" component={MarketingApp}></Route>
+                        <Route path="/auth" component={AuthLazy}></Route>
+                        <Route path="/" component={MarketingLazy}></Route>
                     </Switch>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </StylesProvider>
